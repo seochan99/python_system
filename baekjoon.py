ec3e9c39d271001681ca31877faef75bcd31ddf4
seochan99 <gmlcks0513@naver.com>
2021-08-02 20:40:46 +0900

    2754

--- baekjoon.py
+++ baekjoon.py
@@ -643,16 +643,27 @@
 #     print(0)
 
 #2476 : 주사위 게임
-n = int(input())
-maxN=[0]*n
-for i in range(n) :
-    arr = list(map(int,input().split()))
-    if arr[0]==arr[1]==arr[2] :
-        maxN[i]=10000+arr[0]*1000
-    elif arr[0]==arr[1] or arr[0]==arr[2] :
-        maxN[i]=1000+arr[0]*100
-    elif arr[1]==arr[2] :
-        maxN[i]=1000+arr[1]*100
-    else :
-        maxN[i]=max(arr)*100
-print(max(maxN))
+# n = int(input())
+# maxN=[0]*n
+# for i in range(n) :
+#     arr = list(map(int,input().split()))
+#     if arr[0]==arr[1]==arr[2] :
+#         maxN[i]=10000+arr[0]*1000
+#     elif arr[0]==arr[1] or arr[0]==arr[2] :
+#         maxN[i]=1000+arr[0]*100
+#     elif arr[1]==arr[2] :
+#         maxN[i]=1000+arr[1]*100
+#     else :
+#         maxN[i]=max(arr)*100
+# print(max(maxN))
+
+#2754 : 학점계산 
+grade = {
+    "A+":4.3,"A0":4.0,"A-":3.7,
+    "B+":3.3,"B0":3.0,"B-":2.7,
+    "C+":2.3,"C0":2.0,"C-":1.7,
+    "D+":1.3,"D0":1.0,"D-":0.7,
+    "F":0.0
+}
+
+print(grade[input()])
