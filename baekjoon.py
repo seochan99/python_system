256913446e7b13bd09772aac969f3bf757dc788f
seochan99 <gmlcks0513@naver.com>
2021-08-02 20:57:25 +0900

    7567

--- baekjoon.py
+++ baekjoon.py
@@ -658,12 +658,22 @@
 # print(max(maxN))
 
 #2754 : 학점계산 
-grade = {
-    "A+":4.3,"A0":4.0,"A-":3.7,
-    "B+":3.3,"B0":3.0,"B-":2.7,
-    "C+":2.3,"C0":2.0,"C-":1.7,
-    "D+":1.3,"D0":1.0,"D-":0.7,
-    "F":0.0
-}
+# grade = {
+#     "A+":4.3,"A0":4.0,"A-":3.7,
+#     "B+":3.3,"B0":3.0,"B-":2.7,
+#     "C+":2.3,"C0":2.0,"C-":1.7,
+#     "D+":1.3,"D0":1.0,"D-":0.7,
+#     "F":0.0
+# }
 
-print(grade[input()])
+# print(grade[input()])
+
+#7567 : 그릇 
+arr = list(str(input()))
+length=10
+for i in range(1,len(arr)):
+    if arr[i-1]==arr[i]:
+        length+=5
+    else :
+        length+=10
+print(length)
