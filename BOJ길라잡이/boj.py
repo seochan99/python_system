7205f087903b4b003c2601bb6c885a509c5dad3f
seochan99 <gmlcks0513@naver.com>
2021-12-17 00:37:08 +0900

    10876

--- BOJ길라잡이/boj.py
+++ BOJ길라잡이/boj.py
@@ -15,10 +15,10 @@
 #         start = mid + 1 
 #     return bsr(target,start,end,data)
 
-num1 = int(input())
-arr1 = list(map(int,input().split()))
-num2 = int(input())
-arr2 = list(map(int,input().split()))
+# num1 = int(input())
+# arr1 = list(map(int,input().split()))
+# num2 = int(input())
+# arr2 = list(map(int,input().split()))
         
 # #이분탐색 
 # arr1.sort()
@@ -46,33 +46,41 @@ arr2 = list(map(int,input().split()))
 #             print(i)
 
 #숫자 카드
-import sys
-input=sys.stdin.readline
-sys.setrecursionlimit(10**4) 
+# import sys
+# input=sys.stdin.readline
+# sys.setrecursionlimit(10**4) 
 
-#이분탐색 
-def bsr(target,start,end,data): #
-    if start>end:
-        return None
-    mid = (start+end)//2
-    if data[mid]==target:
-        return 1
-    elif data[mid]>target:
-        end = mid - 1
-    else : #data[mid]<target
-        start = mid + 1 
-    return bsr(target,start,end,data)
+# #이분탐색 
+# def bsr(target,start,end,data): #
+#     if start>end:
+#         return None
+#     mid = (start+end)//2
+#     if data[mid]==target:
+#         return 1
+#     elif data[mid]>target:
+#         end = mid - 1
+#     else : #data[mid]<target
+#         start = mid + 1 
+#     return bsr(target,start,end,data)
 
-n = int(input()) #상근 카드 갯수 
-arr1 = list(map(int,input().split()))
+# n = int(input()) #상근 카드 갯수 
+# arr1 = list(map(int,input().split()))
 
-m = int(input()) #엠
-arr2 = list(map(int,input().split()))
+# m = int(input()) #엠
+# arr2 = list(map(int,input().split()))
 
-#상근이가 가지고 있으면 1 아니면 0 출력 
-arr1.sort()
-for i in range(m):
-    if bsr(arr2[i],0,n-1,arr1):
-        print(1,end=" ")
-    else :
-        print(0,end=" ")
+# #상근이가 가지고 있으면 1 아니면 0 출력 
+# arr1.sort()
+# for i in range(m):
+#     if bsr(arr2[i],0,n-1,arr1):
+#         print(1,end=" ")
+#     else :
+#         print(0,end=" ")
+
+#10876: 중복빼고 정ㅕ 
+n = int(input())
+arr = list(map(int,input().split()))
+
+#중복제거를 위한 집합으로 받기 
+for i in sorted(list(set(arr))):
+    print(i,end=" ")
