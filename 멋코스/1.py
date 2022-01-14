7010d1cbb3ce4e9d0c9bb40750240d2e78640c76
seochan99 <gmlcks0513@naver.com>
2022-01-14 14:45:27 +0900

    1012

--- 멋코스/1.py
+++ 멋코스/1.py
@@ -432,8 +432,46 @@
 #                 queue.append([cx,cy])
 
 
-
-
 # bfs(0,0)
 # print(matrix[n-1][m-1])
+
+# 1012 : 유기농 배추 
+from collections import deque
+
+
+t = int(input())
+
+dx = [1,-1,0,0]
+dy = [0,0,-1,1]
+
+def bfs(x,y):
+    queue = deque()
+    queue.append([x,y])
+    while queue:
+        a,b = queue.popleft()
+        for i in range(4):
+            cx = a + dx[i]
+            cy = b + dy[i]
+            if 0<=cx<n and 0<=cy<m and matrix[cx][cy]==1:
+                matrix[cx][cy]=0 
+                queue.append([cx,cy])
+
+for _ in range(t):
+    m,n,k=map(int,input().split()) #가로ㅡ세로 
+    matrix = [[0]*m for _ in range(n) ]
+    cnt = 0 
+    #그래프 ㅡ그리기 
+    for _ in range(k):
+        y,x = map(int,input().split())
+        matrix[x][y]=1 #배추심기 
+
+    #탐색 
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j]==1:
+                bfs(i,j) #연결된 요소 변경 
+                matrix[i][j]=0 
+                cnt+=1 
+    print(cnt)
+    
 
