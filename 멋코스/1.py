b67ccde41e764bb98fdb02096d00aac01446d26c
seochan99 <gmlcks0513@naver.com>
2022-01-14 14:03:19 +0900

    1260 prc

--- 멋코스/1.py
+++ 멋코스/1.py
@@ -306,31 +306,68 @@
 # n : 정점 
 # m : 간선 
 # v : 탐색 시작할 정점의 번호 
-from collections import deque
+# from collections import deque
+# import sys 
+# input = sys.stdin.readline
+
+# n,m,v = map(int,input().split())
+
+# matrix = [[0]*(n+1) for _ in range(n+1)]
+# visted = [0 for _ in range(n+1)]
+
+# #BFS 
+# def bfs(v):
+#     queue = deque() 
+#     queue.append(v)
+#     visted[v]=0 
+#     while(queue):
+#         v = queue[0]
+#         print(queue.popleft(),end=" ")
+
+#         for i in range(1,n+1):
+#             if visted[i]==1 and matrix[v][i]==1:
+#                 queue.append(i)
+#                 visted[i]=0 
+
+
+# #DFS 
+# def dfs(v):
+#     print(v,end=" ")
+#     visted[v]=1 
+#     for i in range(1,n+1):
+#         if visted[i]==0 and matrix[v][i]==1:
+#             dfs(i)
+
+# for _ in range(m):
+#     x,y = map(int,input().split())
+#     matrix[x][y]=1 
+#     matrix[y][x]=1  #표시 
+
+# dfs(v)
+# print()
+# bfs(v)
+
+#dfs 
+#bfs 
 import sys 
 input = sys.stdin.readline
+from collections import deque
 
-n,m,v = map(int,input().split())
-
-matrix = [[0]*(n+1) for _ in range(n+1)]
-visted = [0 for _ in range(n+1)]
-
-#BFS 
 def bfs(v):
-    queue = deque() 
+    queue = deque()
     queue.append(v)
-    visted[v]=0 
-    while(queue):
+    visted[v] == 0 
+
+    while queue:
         v = queue[0]
         print(queue.popleft(),end=" ")
-
-        for i in range(1,n+1):
+        for i in range(n+1):
             if visted[i]==1 and matrix[v][i]==1:
                 queue.append(i)
                 visted[i]=0 
 
 
-#DFS 
+
 def dfs(v):
     print(v,end=" ")
     visted[v]=1 
@@ -338,11 +375,17 @@ def dfs(v):
         if visted[i]==0 and matrix[v][i]==1:
             dfs(i)
 
+
+n,m,v = map(int,input().split())
+matrix = [[0]*(n+1) for _ in range(n+1)]
+visted = [0 for _ in range(n+1)]
+
 for _ in range(m):
     x,y = map(int,input().split())
-    matrix[x][y]=1 
-    matrix[y][x]=1  #표시 
+    matrix[x][y] = 1
+    matrix[y][x] = 1
 
 dfs(v)
-print()
+print() 
 bfs(v)
+
