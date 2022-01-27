2736ddcb1e73633567ff2b5dc2827c06f2bf0660
seochan99 <gmlcks0513@naver.com>
2022-01-28 02:07:23 +0900

    js END

---
+++ 생활코딩js/colors.js
@@ -0,0 +1,40 @@
+
+        var Links ={
+            setColor : function(color){
+            // var alist = document.querySelectorAll('a');
+            //     var i = 0;
+            //     while(i<alist.length){
+            //         alist[i].style.color = color;
+            //         i+=1; 
+            // }
+            $('a').css('color',color);
+        }
+        }
+        
+
+        var Body = {
+            setColor:function (color){
+                // document.querySelector('body').style.color = color;
+                $('body').css('color',color);
+            },
+            setBackgroundColor:function (color){
+            $('body').css('backgroundColor',color)
+            }
+        }
+
+        function nightDayHandler(self){
+            var target = document.querySelector('body')
+            if(self.value === 'night'){ 
+                Body.setBackgroundColor('black');
+                Body.setColor('white');
+                self.value = 'day';
+
+                Links.setColor('powderblue');
+            }
+            else{
+                Body.setBackgroundColor('white');
+                Body.setColor('black');
+                self.value = 'night';
+                Links.setColor('blue');
+            }
+        }
