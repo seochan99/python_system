a292abbab379979bd6ced7bfcf8e21bbb5205c21
seochan99 <gmlcks0513@naver.com>
2022-02-04 23:00:01 +0900

    ss

--- 멋코스/5.py
+++ 멋코스/5.py
@@ -174,31 +174,100 @@
 # for i in range(2,n+1):
 #     d[i] = d[i-1]+tp
 
+# 평범한 배낭 : 12865 
 # n 물건 갯수
 # k 최대무게 
+# n,k = map(int,input().split())
+# wvList =[[0,0]]
+# dp = [[0]*(k+1) for _ in range((n+1))]
 
-n,k = map(int,input().split())
-wvList =[[0,0]]
-dp = [[0]*(k+1) for _ in range((n+1))]
-
-for _ in range(n):
-    wvList.append(list(map(int,input().split())))
+# for _ in range(n):
+#     wvList.append(list(map(int,input().split())))
 
 
-#dp[n][k]는 n번째 물건까지 봤을때 무게가 k인 배낭의 최대가치 
-print(dp)
-for i in range(1,n+1):
-    for j in range(1,k+1):
-        w = wvList[i][0]
-        v = wvList[i][1] 
+# #dp[n][k]는 n번째 물건까지 봤을때 무게가 k인 배낭의 최대가치 
+# for i in range(1,n+1):
+#     for j in range(1,k+1):
+#         w = wvList[i][0]
+#         v = wvList[i][1] 
 
-        # 배낭 허용 무게보다 더 큰 무게를 가진아이는 넣지 않는다
-        if j<w: 
-            dp[i][j] = dp[i-1][j]
-        # 배낭허용무게보다 더 작은 무게를 가진 아이라면 ? 
-        else :
-            # 현재 넣을 무게 만큼 배낭에서 빼고 그 물건을 넣는다
-            # 물건 넣지 않고 그대로 가져간다 
-            # 둘중에 큰 가치를 가진 아이 
-            dp[i][j] = max(dp[i-1][j],dp[i-1][j-w]+v) 
-print(dp)            
+#         # 배낭 허용 무게보다 더 큰 무게를 가진아이는 넣지 않는다
+#         if j<w: 
+#             dp[i][j] = dp[i-1][j]
+#         # 배낭허용무게보다 더 작은 무게를 가진 아이라면 ? 
+#         else :
+#             # 현재 넣을 무게 만큼 배낭에서 빼고 그 물건을 넣는다
+#             # 물건 넣지 않고 그대로 가져간다 
+#             # 둘중에 큰 가치를 가진 아이 
+#             dp[i][j] = max(dp[i-1][j],dp[i-1][j-w]+v) 
+# print(dp[n][k])            
+
+
+# 만약 상담이 끝나는 날이 n넘어가면 ㅃㅇ 
+# 그 외에는 비교 
+# 현재 일할때 보상 + 현재 끝낸 다음날 보상 
+# 일 안했을때 보상 
+
+# n = int(input())
+
+# tList = []  #time
+# pList = []  #pay 
+# dp = [0]*(n+1) #dp생성 
+
+# for i in range(n): 
+#     t,p = map(int,input().split())
+#     tList.append(t)
+#     pList.append(p)
+
+# for i in range(n-1,-1,-1): #뒤에서 부터 검사 
+#     if tList[i]+i>n: 
+#         dp[i]=dp[i+1]
+#     else :
+#         dp[i] = max(dp[i+1],pList[i]+dp[i+tList[i]])
+# print(dp[0])
+
+
+# n = int(input())
+# score = [0 for _ in range(301)]
+# dp = [0 for _ in range(301)]
+
+# for i in range(n):
+#     score[i] = int(input())
+
+# dp[0] = score[0]
+# dp[1] = score[0] + score[1]
+# dp[2] = max(score[0] + score[2],score[1]+score[2]) 
+
+# for i in range(3,n):
+#     #마지막 계딴의 전계단을 밟음
+#     #마지막 계단 전계단을 안밟음 
+#     dp[i] = max(dp[i-2]+score[i],dp[i-3]+score[i-1]+score[i])
+
+
+#1. 계단 한개씩 또는 두개씩 
+#연속 세 계단 x 
+#마지막 계단 밟기 
+import sys 
+input = sys.stdin.readline
+n = int(input())
+
+score = [0 for _ in range(301)]
+dp = [0 for _ in range(301)]
+
+for i in range(n):
+    score[i] = int(input())
+
+#첫번째 계단 
+dp[0] = score[0]
+#두번째 계딴 
+dp[1] = score[0]+score[1]
+#세번째 계단 
+dp[2] = max(score[0]+score[2],score[1]+score[2])
+
+#전계단을 밟을 경우 :
+#전계단을 밟지 않을 경우 :
+for i in range(3,n):
+    dp[i] = max(dp[i-3]+score[i-1]+score[i],dp[i-2]+score[i])
+
+
+print(dp[n-1])
--- 코드라이언/파이썬 심화/crawling.py
+++ 코드라이언/파이썬 심화/crawling.py
@@ -1,2 +1,30 @@
-import requests
+import smtplib
+from email.message import EmailMessage
+import imghdr
+import re
 
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 465
+
+message = EmailMessage()
+message.set_content("코드라이언 수업중입니다.")
+
+message["Subject"] = "이것은 제목입니다."
+message["From"] = "###@gmail.com"
+message["To"] = "###@gmail.com"
+
+with open("codelion.png","rb") as image:
+    image_file = image.read()
+
+image_type = imghdr.what('codelion',image_file)
+message.add_attachment(image_file,maintype='image',subtype=image_type)
+
+smtp = smtplib.SMTP_SSL(SMTP_SERVER,SMTP_PORT)
+smtp.login("###@gmail.com","######")
+
+# 소문자 a-z 대문자 A-Z : 2~3
+reg = "^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$"
+print(re.match(reg,"codelion.examplegmailcom"))
+
+# smtp.send_message(message)
+# smtp.quit()
