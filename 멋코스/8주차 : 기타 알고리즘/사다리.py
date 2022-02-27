d721cba224b953dae2dc28a5003aea9f2173b940
seochan99 <gmlcks0513@naver.com>
2022-02-27 14:48:22 +0900

    Update 사다리.py

--- 멋코스/8주차 : 기타 알고리즘/사다리.py
+++ 멋코스/8주차 : 기타 알고리즘/사다리.py
@@ -1,11 +1,11 @@
-import random
+# import random
 
-arr1 = ["박현준","서희찬","이건회","이슬기"]
-arr2 = [1,2,3,4]
+# arr1 = ["박현준","서희찬","이건회","이슬기"]
+# arr2 = [1,2,3,4]
 
-random.shuffle(arr1)
+# random.shuffle(arr1)
 
-print(arr1) ; print(arr2)
+# print(arr1) ; print(arr2)
 
 # import random
 
@@ -16,3 +16,25 @@ print(arr1) ; print(arr2)
 # random.shuffle(arr2)
 
 # print(arr1) ; print(arr2)
+
+# def fibo(n):
+#     if n==0:
+#         return 0
+#     if n==1:
+#         return 1
+#     return fibo(n-1)+fibo(n-2)
+
+# print(fibo(7))
+
+# def gop(x,y):
+#     return x*y 
+# print(gop(3,5))
+
+# print((lambda x,y : x*y)(3,5))
+
+# fruit = {'red':"apple,strewberry",'yellow':'banana'}
+# print(fruit['red'])
+listss = list([1,2,3,3,3,4,5])
+jiphap = set([1,2,3,3,3,5])
+
+print(listss) ; print(jiphap)
