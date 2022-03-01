f2cbab6553fb780b7789f72ee94aed52ff819d27
seochan99 <gmlcks0513@naver.com>
2022-03-02 02:15:03 +0900

    Update 사다리.py

--- 멋코스/8주차 : 기타 알고리즘/사다리.py
+++ 멋코스/8주차 : 기타 알고리즘/사다리.py
@@ -1,12 +1,14 @@
-import random
+## 사다리 알고리즘 
 
-arr1 = ["서희찬" ,"김수영","이건회","정준홍","이여원","오광혁", "정민경","안석환","백지원"]
-arr2 = [1,1,2,2,3,3,4,4,4]
+# import random
 
-random.shuffle(arr1)
+# arr1 = ["서희찬" ,"김수영","이건회","정준홍","이여원","오광혁", "정민경","안석환","백지원"]
+# arr2 = [1,1,2,2,3,3,4,4,4]
+
+# random.shuffle(arr1)
 
 
-for i in range(9):
-    print(f"{arr2[i]}팀 : {arr1[i]}")
+# for i in range(9):
+#     print(f"{arr2[i]}팀 : {arr1[i]}")
 
 ## 화이팅~~~ 홧팅~~~~ 
