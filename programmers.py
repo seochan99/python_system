3a74211ce679c363b9d92a2e4049ac4203bf3e89
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-05-12 11:49:59 +0900

    ho

--- programmers.py
+++ programmers.py
@@ -1,13 +1,13 @@
-# ## 프로그래머스 level 1 : x만큼 간격 
+# ## 프로그래머스 level 1 : x만큼 간격
 
-# # #내풀이 
+# # #내풀이
 # # def solution(x,n):
 # #     answer = []
 # #     for i in range(n):
 # #         answer.append(x*(i+1))
 # #     return answer
 
-# # #다른사람풀이 
+# # #다른사람풀이
 # # def solution(x,n):
 # #     return [i*x+x for i in range(n)]
 
@@ -18,7 +18,7 @@
 
 # # print(solution("01023624076"))
 
-# #행렬의 덧셈 
+# #행렬의 덧셈
 # # def solution(arr1, arr2):
 # #     answer = [[]]
 # #     answer.append(arr1[0]+arr2[0])
@@ -33,166 +33,165 @@
 # # print("나머지 :",num1%num2)
 
 
-# #문자열 압축 
+# #문자열 압축
 
 # # def solution(arr):
-# #     ans = len(arr)  # 처음 길이 
-# #     for step in range(1,len(arr)//2+1): #문자열 길이의 절반만큼만 잘라서 확인 ! 그 이후부터는 잘라도 다음 조각이 없음 
-# #         com = "" #빈문자열 
-# #         comArr = arr[0:step] 
+# #     ans = len(arr)  # 처음 길이
+# #     for step in range(1,len(arr)//2+1): #문자열 길이의 절반만큼만 잘라서 확인 ! 그 이후부터는 잘라도 다음 조각이 없음
+# #         com = "" #빈문자열
+# #         comArr = arr[0:step]
 # #         cnt = 1
 # #         for j in range(step,len(arr),step):
-# #             if comArr == arr[j:j+step]: #문자열이 동일하다면 
-# #                 cnt+=1 
-# #             else: #다른 문자열 나오면 com완성하고 상태초기화 
-# #                 com += str(cnt) + comArr if cnt>=2 else comArr #2이상일때마다, 1은 생략하니깐 ! 
+# #             if comArr == arr[j:j+step]: #문자열이 동일하다면
+# #                 cnt+=1
+# #             else: #다른 문자열 나오면 com완성하고 상태초기화
+# #                 com += str(cnt) + comArr if cnt>=2 else comArr #2이상일때마다, 1은 생략하니깐 !
 # #                 comArr = arr[j:j+step]
-# #                 cnt = 1  # 
-# #         #남아 있는 문자열 붙이기 
-# #         com += str(cnt)+ comArr if cnt>=2 else comArr 
+# #                 cnt = 1  #
+# #         #남아 있는 문자열 붙이기
+# #         com += str(cnt)+ comArr if cnt>=2 else comArr
 # #         ans = min(ans,len(com))
-# #     return ans 
+# #     return ans
 # # arr = input()
 
 # # print(solution(arr))
 
 # #자물쇠..
-# #완전 탐색 
+# #완전 탐색
 
-# #matrix 90도 회전 함수 
-# # def rotate_90(a): 
+# #matrix 90도 회전 함수
+# # def rotate_90(a):
 # #     n = len(a)
 # #     m = len(a[0])
 # #     result = [[0]*n for _ in range(m)]
 # #     for i in range(n):
 # #         for j in range(m):
-# #             result[j][n-i-1] = a[i][j] 
-# #     return result 
-# # #모두 1인지 확인 함수 
-# # def check(new_lock): 
+# #             result[j][n-i-1] = a[i][j]
+# #     return result
+# # #모두 1인지 확인 함수
+# # def check(new_lock):
 # #     lock_length = len(new_lock) // 3
 # #     for i in range(lock_length,lock_length * 2):
 # #         for j in range(lock_length,lock_length*2):
 # #             if new_lock[i][j] != 1:
-# #                 return False 
-# #     return True 
+# #                 return False
+# #     return True
 # # def solution(key,lock):
 # #     n = len(lock)
 # #     m = len(key)
-# #     new_lock = [[0]*(n*3) for _ in range(n*3)] #새로운 매트릭스 생성 
-# #     #새 매트릭스 가운데 넣기 
+# #     new_lock = [[0]*(n*3) for _ in range(n*3)] #새로운 매트릭스 생성
+# #     #새 매트릭스 가운데 넣기
 # #     for i in range(n):
 # #         for j in range(m):
-# #             new_lock[i+n][j+n] = lock[i][j] 
-    
-# #     for rotation in range(4): 
-# #         key = rotate_90(key) 
+# #             new_lock[i+n][j+n] = lock[i][j]
+
+# #     for rotation in range(4):
+# #         key = rotate_90(key)
 # #         for x in range(n*2):
 # #             for y in range(n*2):
 # #                 for i in range(m):
 # #                     for j in range(m):
-# #                         #열쇠 넣기 
+# #                         #열쇠 넣기
 # #                         new_lock[x+i][y+j]+=key[i][j]
-# #                 #확인하기 
+# #                 #확인하기
 # #                 if check(new_lock) == True :
-# #                     return True 
-# #                 # False 이면 열쇠다시빼기 
+# #                     return True
+# #                 # False 이면 열쇠다시빼기
 # #                 for i in range(m):
 # #                     for j in range(m):
 # #                         new_lock[x+i][y+j] -= key[i][j]
-# #     return False 
+# #     return False
 
-# #3190 : 뱀 
+# #3190 : 뱀
 # # from collections import deque
 
 # # #상 우 하 좌
 # # dy = [-1,0,1,0]
 # # dx = [0,1,0,-1]
 # # def change(direct,c):
-# #     # 상 0 
+# #     # 상 0
 # #     # 우 1
 # #     # 하 2
-# #     # 좌 3 
-# #     # 시계방향 회전 : 상(0) -> 우(1) -> 하(2) -> 좌(3) -> 상(0) : +1 방향 
-# #     # 반시꼐 방향 회전 : 상(0) -> 좌(3) ... 
+# #     # 좌 3
+# #     # 시계방향 회전 : 상(0) -> 우(1) -> 하(2) -> 좌(3) -> 상(0) : +1 방향
+# #     # 반시꼐 방향 회전 : 상(0) -> 좌(3) ...
 # #     if c == "L":
-# #         direct = (direct - 1) % 4 
+# #         direct = (direct - 1) % 4
 # #     else :
-# #         direct = (direct + 1) % 4 
+# #         direct = (direct + 1) % 4
 # #     return direct
 
 # # def start():
-# #     direct = 1 #초기 방향 
-# #     time = 1 #게임시간 
-# #     y,x = 0,0 #초기 뱀 위치 
+# #     direct = 1 #초기 방향
+# #     time = 1 #게임시간
+# #     y,x = 0,0 #초기 뱀 위치
 # #     visted = deque([[y,x]])
 # #     matrix[y][x] = 2
 # #     while True :
 # #         y,x = y+dy[direct], x+dx[direct]
 # #         if 0<=y<n and 0<=x<n and matrix[y][x] != 2:
-# #             if not matrix[y][x] == 1: #apple X 
+# #             if not matrix[y][x] == 1: #apple X
 # #                 temp_y,temp_x = visted.popleft()
-# #                 matrix[temp_y][temp_x] = 0 # del tail 
+# #                 matrix[temp_y][temp_x] = 0 # del tail
 # #             matrix[y][x] = 2
 # #             visted.append([y,x])
-# #             if time in times.keys(): #시간초에 도달하면 
-# #                 direct = change(direct,times[time]) #방향과 바꿔야하는 방향 전달 
+# #             if time in times.keys(): #시간초에 도달하면
+# #                 direct = change(direct,times[time]) #방향과 바꿔야하는 방향 전달
 # #             time += 1
-# #         else : #벽에 부딪힘 ㅜㅜ 
+# #         else : #벽에 부딪힘 ㅜㅜ
 # #             return time
 
 
-
-# # n = int(input()) #보드크기 
-# # k = int(input()) #사과 개수 
+# # n = int(input()) #보드크기
+# # k = int(input()) #사과 개수
 
 # # matrix = [[0]*n for _ in range(n)]
 # # kXY =[]
 # # for _ in range(k):
 # #     x,y = map(int,input().split())
-# #     matrix[x-1][y-1] = 1 #save apple 
+# #     matrix[x-1][y-1] = 1 #save apple
 
-# # l = int(input()) #방향전환 
+# # l = int(input()) #방향전환
 
 # # times = {}
 
 # # for _ in range(l):
 # #     x,c = input().split()
-# #     times[int(x)] = c 
+# #     times[int(x)] = c
 
 # # print(start())
 
-# # def search_binary(list, value): 
-# #     low = 0 
-# #     high = len(list)-1 
+# # def search_binary(list, value):
+# #     low = 0
+# #     high = len(list)-1
 
-# #     while low <= high: 
+# #     while low <= high:
 # #         middle = (low+high)//2
 # #         print(f"low : {low}  middle : {middle} high : {high} list[middle] : {list[middle]}")
 
-# #         if list[middle] > value: 
-# #             high = middle-1 
+# #         if list[middle] > value:
+# #             high = middle-1
 # #         elif list[middle] < value:
-# #             low = middle+1 
-# #         else: 
-# #             return middle 
-# #     return -1 
+# #             low = middle+1
+# #         else:
+# #             return middle
+# #     return -1
 
-# # myList = [ 2, 6, 11, 13, 18, 20, 22, 27, 29, 30, 34, 38, 41, 42, 45, 47] 
-# # print("인덱스=", search_binary(myList, 34)) 
+# # myList = [ 2, 6, 11, 13, 18, 20, 22, 27, 29, 30, 34, 38, 41, 42, 45, 47]
+# # print("인덱스=", search_binary(myList, 34))
 
-# # from random import * 
-# # from math import sqrt 
+# # from random import *
+# # from math import sqrt
 
 # # n = int(input("반복횟수를 입력하시오 : "))
 
 # # inside = 0
 # # for i in range(0,n):
-# #     x = random() # 0~1.0 
+# #     x = random() # 0~1.0
 # #     y = random()
-# #     if sqrt(x*x+y*y)<=1: # 거리 1.0이하 
+# #     if sqrt(x*x+y*y)<=1: # 거리 1.0이하
 # #         # print(f"x : {x} y : {y} 는 원 내부의 점 입니다.")
-# #         inside+=1 
+# #         inside+=1
 # #     # else :
 # #         # print(f"x : {x} y : {y} 는 원 외부의 점 입니다.")
 # # pi = 4 * inside / n
@@ -201,7 +200,7 @@
 
 # # print( 5/10 *4)
 
-# # # from random import * 
+# # # from random import *
 # # # from math import sqrt
 
 # # # n = int(input("반복횟수를 입력하시오: "))
@@ -268,3 +267,59 @@
 #     '48' : ['47','49'],
 #     '49' : ['48'],
 # }
+
+# a = 5
+# b = 3
+# result = a+b
+
+# print(f"더하기 결과는 : {a+b}")
+
+# print("빼기 결과는 : ", a-b)
+
+# print(f"곱하기 결과는 : {a*b}")
+
+# print(f"/ 나누기 결과는 : {a/b}")
+# print(f"// 나누기 결과는 : {a//b}")
+
+# print(f"나머지 결과는 :{a%b}")
+
+# print(f"제곱 결과는 : {a**2}")
+
+from ast import Or
+
+
+a = 4
+b = 6
+
+# 비교연산자
+# a랑 b비교 출력 코드
+# == : 동일한가?
+# != : 동일하지않는가?
+# >= <= < >
+# print(a >= b)
+# print(a >= b)
+# print(a <= b)
+
+# 할당연산자
+# a = a + 1
+# a++
+# ++a
+# a+=1
+# a /= 1
+
+# 논리연산자
+# a = True
+# b = False
+# AND : and
+# &&
+# OR : or
+# ||
+
+# print(a and b)
+# print(a or b)
+
+# \n : 개행
+# \t : 탭
+
+
+print('hello world')
--- 사다리.py
+++ 사다리.py
@@ -1,14 +1,45 @@
-# # 사다리 알고리즘 
+# 사다리 알고리즘
 
-# import random
+import random
 
-# arr1 = ["서희찬" ,"김수영","이건회","정준홍","이여원","오광혁", "정민경","안석환","백지원"]
-# arr2 = [1,1,2,2,3,3,4,4,4]
+# 선발대 운영진
+staffFirst = ["김태연", "백지원", "서희찬", "이슬기", "정민경", "이건회", "신예진", "안석환"]
+staffFirstTeam = [1, 1, 2, 2, 3, 3, 4, 4]
 
-# random.shuffle(arr1)
+# 후발대 운영진
+staffLast = ["강동희", "김수영", "정준홍", "안소은", "박상준"]
+staffLastTeam = [1, 2, 3, 4, 4]
 
+# 선발대 학생
+studentFirst = ["김성준", "박영신", "오민영", "오준서", "유다현", "윤영서",
+                "이영서", "한수연", "한주아", "홍세림", "이예나", "안유성", "이유진"]
+studentFristTeam = [1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4]
 
-# for i in range(9):
-#     print(f"{arr2[i]}팀 : {arr1[i]}")
+# 후발대 학생
+studentLast = ["김나연", "김재니", "이상돈", "서연미", "장여신", "이지영"]
+studentLastTeam = [1, 1, 2, 2, 3, 4]
 
-# 화이팅~~~ 홧팅~~~~ ㅌㅌㅂ
+random.shuffle(staffFirst)
+random.shuffle(staffLast)
+random.shuffle(studentFirst)
+random.shuffle(studentLast)
+
+print("-------운영진 선발대-------")
+
+for i in range(8):
+    print(f"{staffFirstTeam[i]}팀 : {staffFirst[i]}")
+
+print("-------운영진 후발대-------")
+
+for i in range(5):
+    print(f"{staffLastTeam[i]}팀 : {staffLast[i]}")
+
+print("-------10기 선발대-------")
+
+for i in range(13):
+    print(f"{studentFristTeam[i]}팀 : {studentFirst[i]}")
+
+print("-------10기 후발대-------")
+
+for i in range(6):
+    print(f"{studentLastTeam[i]}팀 : {studentLast[i]}")
