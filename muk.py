3edd9b2bae1071bf56d872ba303410ef313d566e
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-06-09 14:30:22 +0900

    Update muk.py

--- muk.py
+++ muk.py
@@ -1,9 +1,9 @@
-import random
+# import random
 
-staffFirst = ["김치만", "은하수", "남산골", "산타돈부리", "닭칼국수", "홍짜장"]
-staffFirstTeam = [1, 2, 3, 4, 5, 6]
+# staffFirst = ["김치만", "은하수", "남산골", "산타돈부리", "닭칼국수", "홍짜장"]
+# staffFirstTeam = [1, 2, 3, 4, 5, 6]
 
-random.shuffle(staffFirst)
+# random.shuffle(staffFirst)
 
-for i in range(1):
-    print(f"갈 곳 : {staffFirst[i]}")
+# for i in range(1):
+#     print(f"갈 곳 : {staffFirst[i]}")
