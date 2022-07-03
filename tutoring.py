3d3e7a73f546d6ca9f4a540da2200650bfa29c18
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-07-03 10:44:32 +0900

    ss

--- tutoring.py
+++ tutoring.py
@@ -104,7 +104,61 @@ import re
 #     print()
 # 0 : False
 # 1 2 3 ... : True
+
+# def menu():
+#     print("---계산기---")
+#     print("1. 더하기")
+#     print("2. 빼기")
+#     print("3. 나누기")
+#     print("4. 곱하기")
+#     print("5. 나머지")
+#     print("6. 종료")
+#     print("----------")
+
+
+# def adder(num1, num2):
+#     return num1+num2
+
+
+# def sub(num1, num2):
+#     return num1-num2
+
+
+# def div(num1, num2):
+#     return num1//num2
+
+
+# def mul(num1, num2):
+#     return num1*num2
+
+
+# def rest(num1, num2):
+#     return num1 % num2
+
+
 # while True:
+#     menu()
+#     num1, num2 = map(int, input("정수 두개를 입력해주세요 : ").split())
+#     choice = int(input("선택지를 입력해주세요 : "))
+
+#     if choice == 1:
+#         print(f"더하기 값 : {adder(num1, num2)}")
+#     elif choice == 2:
+#         print(f"빼기 값 : {sub(num1, num2)}")
+#     elif choice == 3:
+#         print(f"나누기 값 : {div(num1, num2)}")
+#     elif choice == 4:
+#         print(f"곱하기 값 : {mul(num1, num2)}")
+#     elif choice == 5:
+#         print(f"나머지 값 : {rest(num1, num2)}")
+#     elif choice == 6:
+#         print("프로그램을 종료합니다.")
+#         break
+#     else:
+#         print("1~5사이의 숫자를 입력해주세요.")
+
+# while True:
+
 #     print("---계산기---")
 #     print("1. 더하기")
 #     print("2. 빼기")
@@ -116,7 +170,6 @@ import re
 
 #     num1, num2 = map(int, input("정수 두개를 입력해주세요 : ").split())
 #     choice = int(input("선택지를 입력해주세요 : "))
-
 #     if choice == 1:
 #         print(f"더하기 값 : {num1+num2}")
 #     elif choice == 2:
@@ -131,6 +184,92 @@ import re
 #         print("프로그램을 종료합니다.")
 #     else:
 #         print("1~5사이의 숫자를 입력해주세요.")
+
+
+# # 1
+# def adder(num1, num2):
+#     return num1+num2
+
+
+# def sub(num1, num2):
+#     return num1-num2
+
+
+# def div(num1, num2):
+#     return num1//num2
+
+
+# def mul(num1, num2):
+#     return num1*num2
+
+
+# def rest(num1, num2):
+#     return num1 % num2
+
+
+# def menu(choice):
+#     if choice == 1:
+#         print(f"더하기 값 : {adder(num1, num2)}")
+#     elif choice == 2:
+#         print(f"빼기 값 : {sub(num1, num2)}")
+#     elif choice == 3:
+#         print(f"나누기 값 : {div(num1, num2)}")
+#     elif choice == 4:
+#         print(f"곱하기 값 : {mul(num1, num2)}")
+#     elif choice == 5:
+#         print(f"나머지 값 : {rest(num1, num2)}")
+#     else:
+#         print("잘못된 값을 입력하셨씁니다.")
+
+
+# while True:
+#     print("---계산기---")
+#     print("1. 더하기")
+#     print("2. 빼기")
+#     print("3. 나누기")
+#     print("4. 곱하기")
+#     print("5. 나머지")
+#     print("6. 종료")
+#     print("----------")
+#     num1, num2 = map(int, input("정수 두개를 입력해주세요 : ").split())
+#     choice = int(input("선택지를 입력해주세요 : "))
+#     menu(choice)
+
+
+# 2
+# while True:
+#     print("---계산기---")
+#     print("1. 더하기")
+#     print("2. 빼기")
+#     print("3. 나누기")
+#     print("4. 곱하기")
+#     print("5. 나머지")
+#     print("6. 종료")
+#     print("----------")
+
+
+# def adder(num1, num2, choice):
+#     num1, num2 = map(int, input("정수 두개를 입력해주세요 : ").split())
+#     choice = int(input("선택지를 입력해주세요 : "))
+#     if choice == 1:
+#         print(f"더하기 값 : {num1+num2}")
+#     elif choice == 2:
+#         print(f"빼기 값 : {num1-num2}")
+#     elif choice == 3:
+#         print(f"나누기 값 : {num1//num2}")
+#     elif choice == 4:
+#         print(f"곱하기 값 : {num1*num2}")
+#     elif choice == 5:
+#         print(f"나머지 값 : {num1%num2}")
+#     elif choice == 6:
+#         print("프로그램을 종료합니다.")
+#     else:
+#         print("1~5사이의 숫자를 입력해주세요.")
+#     return
+
+
+# print(adder)
+
 
 # for 구구단
 # for j in range(1, 10):
@@ -167,3 +306,18 @@ import re
 
 # print(list(range(10, 2, -1)))
 # p
+
+
+n = int(input())
+
+han = 0
+
+for i in range(1, n + 1):
+    if i < 100:
+        han += 1
+    else:
+        ns = list(map(int, str(i)))
+        print(ns)
+        if ns[0] - ns[1] == ns[1] - ns[2]:
+            han += 1
+print(han)
